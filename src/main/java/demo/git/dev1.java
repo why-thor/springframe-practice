deb4b0964cba6b67a49ab66927b583ef658f93d7
why-thor <286763969@qq.com>
2020-05-15 01:38:41 +0800

    dev第一次提交

---
+++ src/main/java/demo/git/dev1.java
@@ -0,0 +1,13 @@
+package demo.git;
+
+/**
+ * Created by Administrator on 2020/5/15/015.
+ */
+public class dev1 {
+    public static void main(String[] args) {
+        System.out.println("dev1");
+    }
+
+
+}
+
