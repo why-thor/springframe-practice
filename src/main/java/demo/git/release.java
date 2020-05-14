0fdeb528d5542ec38cf66ef98b290aab716d2acf
why-thor <286763969@qq.com>
2020-05-15 01:40:18 +0800

    release第一次提交
    (cherry picked from commit fc263ff)
    
    合并到dev1

---
+++ src/main/java/demo/git/release.java
@@ -0,0 +1,12 @@
+package demo.git;
+
+/**
+ * Created by Administrator on 2020/5/15/015.
+ */
+public class release {
+    public static void main(String[] args) {
+           System.out.println("release01");
+
+    }
+
+}
