c30ae97c20c0207f16299f65b413c477953b1f77
why-thor <286763969@qq.com>
2020-05-15 01:20:31 +0800

    master第一次提交

---
+++ src/main/java/demo/generic/GenericFactoryImpl.java
@@ -0,0 +1,15 @@
+package demo.generic;
+
+public class GenericFactoryImpl<N,T> implements GenericIFactory<T, N> {
+
+    @Override
+    public T nextObject() {
+        return null;
+    }
+
+
+    @Override
+    public N nextNumber() {
+        return null;
+    }
+}
