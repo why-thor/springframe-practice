4c43fa5ebf0fe604502976e62ad757cd2c7ea54a
why-thor <286763969@qq.com>
2020-05-15 01:47:57 +0800

    release03

--- src/main/java/demo/git/release.java
+++ src/main/java/demo/git/release.java
@@ -7,6 +7,9 @@ public class release {
     public static void main(String[] args) {
            System.out.println("release01");
            System.out.println("release02");
+           System.out.println("release03");
+           
+
 
 
     }
